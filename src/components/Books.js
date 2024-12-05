bdea86e29ba54c16535e64571ac902e3cae8026a
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2024-12-05 12:43:48 -0500

    feature/Books: Search with query params

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { BooksContext } from "../context/BooksContext";
+import { useLocation } from "react-router-dom";
 
 export default function Books() {
+  const query = new URLSearchParams(useLocation().search);
+  const search = query.get("search")?.toLowerCase() ?? "";
   const books = useContext(BooksContext);
+  const filteredBooks = books.filter((book) => {
+    return (
+      book.title.toLowerCase().includes(search) ||
+      book.author.toLowerCase().includes(search)
+    );
+  });
 
   return (
     <div>
       <h1>Books</h1>
 
+      {filteredBooks.length === 0 && <div>No books match this search.</div>}
+
       <ul>
-        {books.map((book) => {
+        {filteredBooks.map((book) => {
           return (
             <li key={book.id}>
               {book.title} by {book.author}
