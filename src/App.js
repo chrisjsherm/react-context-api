d39fcd7ec32a1dd66cfe24595f163071a069f10d
Christopher Sherman <chrisjsherm@users.noreply.github.com>
2024-12-05 12:31:20 -0500

    feature/BookDetails

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import BookDetails from "./components/BookDetails";
 import Books from "./components/Books";
 import { BooksProvider } from "./context/BooksContext";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 function App() {
   return (
     <BooksProvider>
-      <Books></Books>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/books"></Navigate>}></Route>
+          <Route path="/books" element={<Books></Books>}></Route>
+          <Route
+            path="/books/:bookId"
+            element={<BookDetails></BookDetails>}
+          ></Route>
+        </Routes>
+      </Router>
     </BooksProvider>
   );
 }
---
+++ src/components/BookDetails.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useContext } from "react";
+import { BooksContext } from "../context/BooksContext";
+import { useParams } from "react-router-dom";
+
+export default function BookDetails() {
+  const { bookId } = useParams();
+  const books = useContext(BooksContext);
+  const book = books.find((book) => book.id === Number.parseInt(bookId, 10));
+
+  if (books.length === 0) {
+    return <div>Loading&hellip;</div>;
+  }
+
+  if (!book) {
+    return <h1>Not Found</h1>;
+  }
+
+  return (
+    <>
+      <h1>{book.title}</h1>
+
+      <h2>By {book.author}</h2>
+
+      <p>{book.description}</p>
+    </>
+  );
+}
